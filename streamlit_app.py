1a3e9303a4e2f654f69a67a4b8daaa0c7e6c460b
duende85 <121720880+duende85@users.noreply.github.com>
2024-08-18 10:04:46 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -171,14 +171,4 @@ for i, (note, style, is_active, label) in enumerate(keys_layout):
 st.markdown("## Write anything you want below the piano here.")
 st.write("This is where you can add any text, charts, or other content you want to display below the piano visualization.")
 
-image_path = KEY_SCORES['F6']
-
-# Custom HTML to center the image
-centered_image_html = f"""
-<div style="display: flex; justify-content: center;">
-    <img src="{image_path}" alt="F6 Chord" style="width:auto;">
-</div>
-"""
-
-# Display the centered image in Streamlit
-st.markdown(centered_image_html, unsafe_allow_html=True)
+st.image(KEY_SCORES['F6'], caption="F6 Chord", use_column_width=False)
