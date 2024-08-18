957b66f8d06bc7914c40bbdfe9c93f51a00efd2c
duende85 <121720880+duende85@users.noreply.github.com>
2024-08-18 08:11:16 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,48 +8,6 @@ st.set_page_config(layout="wide")
 
 # Define the notes and their corresponding files
 NOTE_FILES = {
-    # Octave 3
-    # 'C3': 'notes/c3.wav',
-    # 'C#3': 'notes/c_sharp_3.wav',
-    # 'D3': 'notes/d3.wav',
-    # 'D#3': 'notes/d_sharp_3.wav',
-    # 'E3': 'notes/e3.wav',
-    # 'F3': 'notes/f3.wav',
-    # 'F#3': 'notes/f_sharp_3.wav',
-    # 'G3': 'notes/g3.wav',
-    # 'G#3': 'notes/g_sharp_3.wav',
-    # 'A3': 'notes/a3.wav',
-    # 'A#3': 'notes/a_sharp_3.wav',
-    # 'B3': 'notes/b3.wav',
-
-    # Octave 4
-    # 'C4': 'notes/c4.wav',
-    # 'C#4': 'notes/c_sharp_4.wav',
-    # 'D4': 'notes/d4.wav',
-    # 'D#4': 'notes/d_sharp_4.wav',
-    # 'E4': 'notes/e4.wav',
-    # 'F4': 'notes/f4.wav',
-    # 'F#4': 'notes/f_sharp_4.wav',
-    # 'G4': 'notes/g4.wav',
-    # 'G#4': 'notes/g_sharp_4.wav',
-    # 'A4': 'notes/a4.wav',
-    # 'A#4': 'notes/a_sharp_4.wav',
-    # 'B4': 'notes/b4.wav',
-
-    # Octave 5
-    # 'C5': 'notes/c5.wav',
-    # 'C#5': 'notes/c_sharp_5.wav',
-    # 'D5': 'notes/d5.wav',
-    # 'D#5': 'notes/d_sharp_5.wav',
-    # 'E5': 'notes/e5.wav',
-    # 'F5': 'notes/f5.wav',
-    # 'F#5': 'notes/f_sharp_5.wav',
-    # 'G5': 'notes/g5.wav',
-    # 'G#5': 'notes/g_sharp_5.wav',
-    # 'A5': 'notes/a5.wav',
-    # 'A#5': 'notes/a_sharp_5.wav',
-    # 'B5': 'notes/b5.wav',
-
     # Octave 6
     'C6': 'notes/c6.wav',
     'C#6': 'notes/c_sharp_6.wav',
@@ -63,20 +21,6 @@ NOTE_FILES = {
     'A6': 'notes/a6.wav',
     'A#6': 'notes/a_sharp_6.wav',
     'B6': 'notes/b6.wav',
-
-    # Octave 7 (future extension)
-    # 'C7': 'notes/c7.wav',
-    # 'C#7': 'notes/c_sharp_7.wav',
-    # 'D7': 'notes/d7.wav',
-    # 'D#7': 'notes/d_sharp_7.wav',
-    # 'E7': 'notes/e7.wav',
-    # 'F7': 'notes/f7.wav',
-    # 'F#7': 'notes/f_sharp_7.wav',
-    # 'G7': 'notes/g7.wav',
-    # 'G#7': 'notes/g_sharp_7.wav',
-    # 'A7': 'notes/a7.wav',
-    # 'A#7': 'notes/a_sharp_7.wav',
-    # 'B7': 'notes/b7.wav',
 }
 
 def play_note(note):
@@ -112,6 +56,7 @@ white_key_style = """
         justify-content: center;
         align-items: center;
         margin: 0 2px;
+        position: relative;
     }
     </style>
     """
@@ -152,6 +97,6 @@ columns = st.columns(len(keys_layout))
 
 for i, (note, style) in enumerate(keys_layout):
     with columns[i]:
-        st.markdown(f'<div class="{style}">{note}</div>', unsafe_allow_html=True)
-        if st.button(note, key=note):
+        if st.button(note, key=note, help=f"Play {note}"):
             play_note(note)
+        st.markdown(f'<div class="{style}">{note}</div>', unsafe_allow_html=True)
